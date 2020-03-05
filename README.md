da353b716c0f4f002b16f7befc59c13261463b61
BestReder <997594560@qq.com>
2020-03-05 13:55:29 +0800

    v2.0

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # cscan
 
-基于[httpscan](https://github.com/zer0h/httpscan#httpscan)的多端口c段扫描器。
+基于`python3`和[httpscan](https://github.com/zer0h/httpscan#httpscan)的多端口c段扫描器。
 
-**用法**：`python2 cscan.py IP/CIDR –t threads`
+![](demo/1.png)
 
-例：`python2 cscan.py 10.20.30.0/24 –t 100`
+**用法**：`python cscan.py -i IP/CIDR –t threads`
 
+**例**：`python cscan.py -i 10.20.30.0/24 –t 100`
+
+默认线程为100
+
+![](demo/2.png)
--- cscan.py
+++ cscan.py
@@ -1,86 +1,79 @@
-#!/usr/bin/env python
-#coding:utf-8
-#Author:zjun
-
-import re
-import sys
-import Queue
-import threading
-import optparse
+# cscan based on httpscan
 import requests
-from IPy import IP
+import re
+import IPy
+import sys
+import argparse
+import threading
 
-printLock = threading.Semaphore(1)  #lock Screen print
-TimeOut = 5  #request timeout
 
-#User-Agent
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36','Connection':'close'}
-#Port
-port = [80, 8080, 81, 8081, 7001, 8000, 8088, 8888, 9090, 8090, 88, 8001, 82, 8082, 8089, 9000, 8443, 9999, 8002,
-        89, 999, 8200, 8085, 9001, 9200, 8100, 443, 8087, 8181, 8099, 8899, 9002, 888, 7200, 8020, 7002, 8099, 8800]
+headers={
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36',
+    'Connection':'close'
+}
 
-class scan():
+portlist = [
+    80, 8080, 81, 8081, 7001, 8000, 8088, 8888, 9090, 8090, 88, 8001, 82, 9080, 8082, 8089, 9000, 8443, 9999, 8002,
+    89, 8083, 8200, 90, 8086, 801, 8011, 8085, 9001, 9200, 8100, 8012, 85, 8084, 8070, 8091, 8003, 7777, 8010, 443,
+    8028, 8087, 83, 10000, 8181, 8099, 8899, 8360, 8300, 9002, 8053, 1000, 8989, 9060, 888, 8006, 6677, 7200, 8280,
+    8161, 8880, 8020, 7070, 889, 1010, 8004, 86, 38501, 41516, 28017, 18080, 7002, 808, 800, 8099, 8800, 8180,
+    3505, 7080, 8484
+]
 
-  def __init__(self,cidr,threads_num):
-    self.threads_num = threads_num
-    self.cidr = IP(cidr)
-	#build ip queue
-    self.IPs = Queue.Queue()
-    for ip1 in self.cidr:
-      for p in port:
-        ip = str(ip1)+':'+str(p)
-        self.IPs.put(ip)
+def iplist(ip):
+    urllist=[]
+    iplist = IPy.IP(ip)
+    for ip in iplist[1:-1]:
+        for port in portlist:
+            urllist.append('http://'+str(ip)+':'+str(port))
+    return urllist
 
-  def request(self):
-    with threading.Lock():
-      while self.IPs.qsize() > 0:
-        ip = self.IPs.get()
+def scan(start, end, urllist, lockObj):
+    for i in range(int(start), int(end)):
         try:
-          r = requests.Session().get('http://'+str(ip),headers=header,timeout=TimeOut)
-          status = r.status_code
-          title = re.search(r'<title>(.*)</title>', r.text) #get the title
-          if title:
-            title = title.group(1).strip().strip("\r").strip("\n")[:30]
-          else:
-            title = "None"
-          banner = ''
-          try:
-            banner += r.headers['Server'][:20] #get the server banner
-          except:pass
-          printLock.acquire()
-          print "|%-21s|%-6s|%-25s|%-35s" % (ip,status,banner,title)
-          
-        except Exception,e:
-          printLock.acquire()
-        finally:
-          printLock.release()
-
-  #Multi thread
-  def run(self):
-    for i in range(self.threads_num):
-      t = threading.Thread(target=self.request)
-      t.start()
-
-if __name__ == "__main__":
-  parser = optparse.OptionParser("Usage: %prog [options] target")
-  parser.add_option("-t", "--thread", dest = "threads_num",
-    default = 100, type = "int",
-    help = "[optional]number of  theads,default=100")
-  (options, args) = parser.parse_args()
-  if len(args) < 1:
-    parser.print_help()
-    sys.exit(0)
-
-                                    
-                                    
-  print "  ___   ___    ___    __ _   _ __  "
-  print " / __| / __|  / __|  / _` | | '_ \ "
-  print "| (__  \__ \ | (__  | (_| | | | | |"
-  print " \___| |___/  \___|  \__,_| |_| |_|\n" 
-  print "Usage: python2 cscan.py [options]\n" 
-  print "Use -h for help\n"
-  print "cscan based on httpscan\n"
-  print "|IP                   |Status|Server                   |Title                               "
-
-  s = scan(cidr=args[0],threads_num=options.threads_num)
-  s.run()
+            n=len(urllist)
+            r = requests.session().get(url=urllist[i],headers=headers,timeout=5)
+            status = r.status_code
+            if status != 404 and status != 403 and status != 400 and status != 502:
+                title = re.search(r'<title>(.*)</title>', r.content.decode())
+                if title:
+                    title = title.group(1).strip().strip("\r").strip("\n")
+                else:
+                    title = 'Null'
+                banner = ''
+                try:
+                    banner += r.headers['Server'][:21]
+                except:
+                    pass
+                lockObj.acquire()
+                print ("|%-29s|%-6s|%-24s|%-35s" % (urllist[i],status,banner,title))
+                lockObj.release()
+        except:
+            pass
+    
+def main():
+    print(r'''
+  ___ ___  ___ __ _ _ __
+ / __/ __|/ __/ _` | '_ \
+| (__\__ \ (_| (_| | | | |
+ \___|___/\___\__,_|_| |_|
+               by:zjun
+            www.zjun.info
+          ''')
+    parser = argparse.ArgumentParser(description='cscan based on httpscan')
+    parser.add_argument('-i', '--ip', required=True, help='target ip or ip segment')
+    parser.add_argument('-t', '--thread', required=False, default = 100, help='number of threads,default = 100')
+    args = parser.parse_args()
+    ip = args.ip
+    numbers = args.thread
+    print ('|IP                           |Status|Server                  |Title')
+    url = iplist(ip)
+    new = len(url) % int(numbers)
+    lock = threading.Lock()
+    for i in range(int(numbers)):
+        if i == int(numbers) - 1:
+            threading.Thread(target=scan, args=(i * len(url) / int(numbers), len(url), url, lock)).start()
+        threading.Thread(target=scan, args=(i * len(url) / int(numbers), (i + 1) * len(url) / int(numbers) - 1, url, lock)).start()
+        
+if __name__ == '__main__':
+    main()
